4a459a497824ef6bbf94ea69a21e5239c02e3c4d
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2024-04-07 16:53:43 -0500

    Primer capitulo completado

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,3 +1,5 @@
+import styles from '@/app/ui/home.module.css';
+<div className={styles.shape} />;
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -17,6 +19,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <div
+            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+          />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
